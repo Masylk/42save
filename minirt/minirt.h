f81a4a137458ff85221ae04fab3b9b406e0a7a4a
Maxime <togbemaxime@gmail.com>
2021-02-15 09:04:21 +0100

    ?

--- minirt/minirt.h
+++ minirt/minirt.h
@@ -1,12 +1,22 @@
 #ifndef MINIRT_H
 # define MINIRT_H
 
+# include <math.h>
+
 typedef	struct	s_vector
 {
-	float	x;
-	float	y;
-	float	z;
-	float	(*length)(struct s_vector a);
+	float	            x;
+	float	            y;
+	float	            z;
+	float               (*length)(struct s_vector a);
+    struct s_vector     (*normal)(struct s_vector a);
 }				t_vector;
+
+typedef struct	s_matrices
+{
+	int	col;
+	int	row;
+	int	**mat;
+}				t_matrices;
 
 #endif
