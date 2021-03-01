c88b279324c9b7b7dd843e924e1add161d063f1c
Maxime TOGBE <mtogbe@e3r9p18.42.fr>
2021-03-01 17:17:13 +0100

    on y arrive

--- minirt/rendering/cylinder.c
+++ minirt/rendering/cylinder.c
@@ -18,14 +18,14 @@ double	check_caps(double t_one, t_ray ray, t_cyl *cyl)
 	if (t < 0 || dot_product(q, q)
 			< ((cyl->width * 0.5) * (cyl->width * 0.5)))
 		return (t_one);
-	if (t > t_one)
+	if (t > t_one && t_one >= 0)
 		t = t_one;
 	t_one = dot_product(sub(top, ray.origin), cyl->v) / denom;
-	q = sub(add(ray.origin, mul_n(ray.direction, t)), top);
+	q = sub(add(ray.origin, mul_n(ray.direction, t_one)), top);
 	if (t_one < 0 || dot_product(q, q)
 			< ((cyl->width * 0.5) * (cyl->width * 0.5)))
 		return (t);
-	if (t > t_one)
+	if (t > t_one && t_one >= 0)
 		t = t_one;
 	return (t);
 }
@@ -35,20 +35,29 @@ double	is_inplanes(double t_one, double t_two, t_ray ray, t_cyl *cyl)
 	t_vector	q;
 	t_vector	p_one;
 	t_vector	p_two;
+	double		t;
 
-	q = add(ray.origin, mul_n(ray.direction, t_one));
+	t = -1;
 	p_one = sub(cyl->coor, mul_n(cyl->v, cyl->height * 0.5));
 	p_two = add(cyl->coor, mul_n(cyl->v, cyl->height * 0.5));
-	if (dot_product(cyl->v, sub(q, p_one)) <= 0
-			|| dot_product(cyl->v, sub(q, p_two)) >= 0)
-		return (-1);
-	q = add(ray.origin, mul_n(ray.direction, t_two));
-	if (dot_product(cyl->v, sub(q, p_one)) <= 0
-			|| dot_product(cyl->v, sub(q, p_two)) >= 0)
-		return (t_one);
-	if (t_one > t_two)
-		return (t_two);
-	return (t_one);
+	if (t_one > 0)
+	{
+		q = add(ray.origin, mul_n(ray.direction, t_one));
+			if (dot_product(cyl->v, sub(q, p_one)) <= 0
+				|| dot_product(cyl->v, sub(q, p_two)) >= 0)
+			return (-1);
+		t = t_one;
+	}
+	if (t_two > 0)
+	{
+		q = add(ray.origin, mul_n(ray.direction, t_two));
+		if (dot_product(cyl->v, sub(q, p_one)) <= 0
+				|| dot_product(cyl->v, sub(q, p_two)) >= 0)
+			return (t);
+		if (t > t_two || t < 0)
+			t = t_two;
+	}
+	return (t);
 }
 
 double	check_disc(double d, t_ray ray, t_cyl *cyl, double a, double b)
@@ -61,12 +70,10 @@ double	check_disc(double d, t_ray ray, t_cyl *cyl, double a, double b)
 	a = 2.0 * a;
 	t_one = (-b + sq) / a;
 	t_two = (-b - sq) / a;
-	if (t_one < 0 && t_two < 0)
+	if (t_one <= 0 && t_two <= 0)
 		return (-1);
 	t_one = is_inplanes(t_one, t_two, ray, cyl);
-	if (t_one < 0)
-		return (-1);
-	return (1);
+	return (t_one);
 	return (check_caps(t_one, ray, cyl));
 }
 //faire une structure t_tools pour les variables des équations
@@ -90,11 +97,11 @@ int	check_cylinder(t_cyl *cyl, t_ray ray)
 			sub(d, mul_n(cyl->v, dot_product(d, cyl->v))));
 	v = sub(d, mul_n(cyl->v, dot_product(d, cyl->v)));
 	c = dot_product(v, v) - ((cyl->width * 0.5) * (cyl->width * 0.5));
-	c = (b * b) + (4 * a * c);
-	if (c <= 0)
+	c = (b * b) - (4 * a * c);
+	if (c < 0)
 		return (0);
 	a = check_disc(c, ray, cyl, a, b);
-	if (a < 0)
+	if (a <= 0)
 		return (0);
 	return (1);
 }
--- minirt/rendering/sphere.c
+++ minirt/rendering/sphere.c
@@ -2,7 +2,7 @@
 
 int	check_spheres(t_data *data, t_ray ray)
 {
-	t_sphere *tmp;
+	t_sphere	*tmp;
 
 	tmp = data->spheres;
 	while (tmp)
@@ -16,24 +16,24 @@ int	check_spheres(t_data *data, t_ray ray)
 
 int	check_sphere(t_sphere *sphere, t_ray ray)
 {
-        double  a;
-        double  b;
-        double  c;
-        double  r;
-        double  delta;
-        t_vector        d;
-        
-        d = sub(ray.origin, sphere->coor);
-        r = sphere->width * 0.5;
-        a = dot_product(ray.direction, ray.direction);
-        b = 2 * dot_product(ray.direction, d);
-        c = dot_product(d, d) - r * r;
-        delta =  b * b - 4 * a * c;
-        if (delta < 0)
-                return (0);
-        delta = sqrt(delta);
-        a = 2 * a;
-        if (((-b + delta) / a) > 0)
-                return (1);
-        return (0);
+	double		a;
+	double		b;
+	double		c;
+	double		r;
+	double		delta;
+	t_vector	d;
+
+	d = sub(ray.origin, sphere->coor);
+	r = sphere->width * 0.5;
+	a = dot_product(ray.direction, ray.direction);
+	b = 2 * dot_product(ray.direction, d);
+	c = dot_product(d, d) - r * r;
+	delta =  b * b - 4 * a * c;
+	if (delta < 0)
+		return (0);
+	delta = sqrt(delta);
+	a = 2 * a;
+	if (((-b + delta) / a) > 0)
+		return (1);
+	return (0);
 }
