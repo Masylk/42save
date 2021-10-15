67965958dba91aafa1ae5322d3ed3475f93e4660
Maxime TOGBE <mtogbe@e3r2p6.42.fr>
2021-10-15 19:09:19 +0200

    oui

--- push_swap/libft/libft.h
+++ push_swap/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 16:22:51 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/07/07 02:28:30 by mtogbe           ###   ########.fr       */
+/*   Updated: 2021/10/15 18:26:19 by mtogbe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ t_list			*ft_lstnew(void *content);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
 void			ft_lstadd_back(t_list **alst, t_list *new);
-void			ft_lstdelone(t_list *lst, void(*del)(void *));
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *));
