8b30d720ef19abb26911d79fb06eba8ee62ad88c
Masylk <togbemaxime@gmail.com>
2023-07-29 08:59:15 +0200

    delete misc

--- common-core/ft_server2/srcs/init.sh
+++
@@ -1,3 +0,0 @@
-if [ "$AUTOINDEX" == "off" ]
-       then echo "auto-index off\n" && sed -Ei "s/autoindex on/autoindex off/" /etc/nginx/sites-available/localhost
-fi
--- common-core/transcendence_box/nest-chat/src/socket/socket.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { SocketEvents } from "./socketEvents";
-import { MatchmakingService } from "./matchmaking.service";
-import { BallService } from "./ball.service";
-
-@Module({
-    providers: [SocketEvents, MatchmakingService, BallService]
-})
-export class SocketModule {}
